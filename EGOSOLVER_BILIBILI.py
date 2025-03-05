59285eaf9e712805ec09630cd2779bb0eb1e1e62
Jup33Q <zxgzg@163.com>
2025-03-05 17:37:50 +0800

    Add files via upload

---
+++ EGOSOLVER_BILIBILI.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+import streamlit.components.v1 as components
+
+components.html(
+    '''
+    <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=114090706014228&bvid=BV1zJ9FYBE3c&cid=28647689751&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+    '''
+)
